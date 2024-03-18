ec9363555c68c794434d66e2c586e17397a93126
samuel-manners <samuel.manners@outlook.com>
2024-03-18 19:44:21 +0000

    Addition of Redis caching

--- searchUtils.js
+++ searchUtils.js
@@ -1,13 +1,31 @@
+//Database connection import
 const createConnection = require('./db');
-const redisClient = require('./redisClient.js');
 
-const searchHandler = async (searchType, searchParam) => {
+//Redis Search Requirements
+const redis = require('redis');
+const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
+const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
+if (!cacheHostName) throw Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
+if (!cachePassword) throw Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
+
+const config =
+{
+    url: `rediss://${cacheHostName}:6380`,
+    password: cachePassword
+};
+
+const cacheConnection = redis.createClient(config);
+
+
+async function searchHandler (searchType, searchParam) {
     if (searchType == "Brand") {
         //Try Cache first, if no response try DB search
         const cacheResults = await cacheSearch(searchParam);
         if (cacheResults == null) {
             const dbResults = await dbSearch('brand', searchParam);
             return dbResults;
+        } else{
+            return cacheResults;
         }
     } else if (searchType == "'Name'") {
         const dbResults = dbSearch('name', searchParam);
@@ -16,27 +34,32 @@ const searchHandler = async (searchType, searchParam) => {
 };
 
 
-const cacheSearch = async (brand) => {
+async function cacheSearch(brand) {
+    await cacheConnection.connect();
     //Look Aside Caching - Ref :https://www.youtube.com/watch?v=krSgKN-5DHs&list=LL&index=1&t=118s
     //TO-DO - Add nameSearch caching
     try {
         const cacheKey = brand;//Design Cache Key Method
-        let cacheEntry = await redisClient.get(cacheKey);
+        let cacheEntry = await cacheConnection.get(cacheKey);
 
         if (cacheEntry) {
-            cacheEntry = JSON.parse(cacheEntry);
-            return { ...cacheEntry, 'source': 'cache' };
+            console.log('Found in Cache');
+            cacheConnection.disconnect();
+            return JSON.parse(cacheEntry);
         } else {
+            console.log('Not found in cache, attempting DB search')
+            cacheConnection.disconnect();
             return null;
         }
 
     } catch (error) {
         console.error('Error retrieving data from cache:', error);
+        cacheConnection.disconnect();
         return null;
     }
 }
 
-const dbSearch = async (searchType, searchParam) => {
+async function dbSearch(searchType, searchParam){
     try {
         if (typeof searchParam === 'undefined') {
             console.log("Search Parameters undefined");
@@ -54,6 +77,12 @@ const dbSearch = async (searchType, searchParam) => {
         connection.end();
 
         if (rows.length > 0) {
+            if (searchType == 'brand'){
+                await cacheConnection.connect();
+                await cacheConnection.set(searchParam, JSON.stringify(rows));
+                console.log('Logged to cache');
+                cacheConnection.disconnect();
+            }
             return rows;
         } else {
             console.log('Error getting Data');
