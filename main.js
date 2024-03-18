e2a0fb7c5de48e6859077cb15491d622cca2c180
samuel-manners <samuel.manners@outlook.com>
2024-03-18 17:26:17 +0000

    Attempting to add Redis to Database Client

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const createConnection = require('./db');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const port = process.env.PORT || 8080;
 const { handleVisionRequest } = require('./AIVision.js');
-
+const redisClient = require('./redisClient.js');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,13 +20,10 @@ app.get('/recycling_database_api', async (req, res) => {
   //Checks SearchType for how to handle and passes request to relevent function
   if (searchType == "'Brand'") {
     const brand = req.query.brand;
-    searchResults = await normalSearch(brand);
+    searchResults = await searchHandler("Brand", brand);
   } else if (searchType == "'Name'") {
     const name = req.query.name;
-    searchResults = await nameSearch(name);
-  } else if (searchType == "'UPC'") {
-    const upc = req.query.UPC;
-    searchResults = await upcSearch(upc)
+    searchResults = await searchHandler("Name", name);
   } else {
     res.status(400).json({ message: 'Bad Request - Type of Search is Invalid' })
   }
@@ -56,7 +55,8 @@ app.post('/cloudvisionapi', async (req, res) => {
   const logo = await handleVisionRequest(req.body.image);
 
   const logoRequest = "'" + logo[0] + "'";
-  const response = await normalSearch(logoRequest);
+  const response = await searchHandler("Brand", logoRequest);
+  console.log(response);
   res.json(response);
 });
 
@@ -64,28 +64,103 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 })
 
+const searchHandler = async (searchType, searchParam) => {
+  if (searchType == "Brand") {
+    //Try Cache first, if no response try DB search
+    const cacheResults = await cacheSearch(searchParam);
+    if (cacheResults == null) {
+      const dbResults = await dbSearch('brand', searchParam);
+      return dbResults;
+    }
+  } else if (searchType == "'Name'") {
+    const dbResults = dbSearch('name', searchParam);
+    return dbResults;
+  }
+};
 
-const normalSearch = async (brand) => {
-  console.log(brand);
 
+const cacheSearch = async (brand) => {
+  //Look Aside Caching - Ref :https://www.youtube.com/watch?v=krSgKN-5DHs&list=LL&index=1&t=118s
+  //TO-DO - Add nameSearch caching
+  console.log('Attempting Cache Search');
   try {
-    if (typeof brand === 'undefined') {
-      console.log("Brand variable is undefined");
+    const cacheKey = brand;//Design Cache Key Method
+    let cacheEntry = await redisClient.get(cacheKey);
+
+    if (cacheEntry) {
+      cacheEntry = JSON.parse(cacheEntry);
+      return { ...cacheEntry, 'source': 'cache' };
+    } else{
       return null;
     }
 
+  } catch (error) {
+    console.error('Error retrieving data from cache:', error);
+    return null;
+  }
+}
+
+const dbSearch = async (searchType, searchParam) => {
+  console.log('Attempting Database Search');
+  try {
+    if (typeof searchParam === 'undefined') {
+      console.log("Search Parameters undefined");
+      return null;
+    } else if (typeof searchType === 'undefined') {
+      console.log("Search Type undefined");
+    }
+
+
     const connection = await createConnection();
-    //Adapted for rough search to work with AI
-    //Ref: https://www.w3schools.com/SQL/sql_like.asp
-    const query = "SELECT UPC, name, description, is_recyclable, packaging_material, brand FROM recyclable_household_items WHERE brand LIKE " + brand;
+    //New style search query, single function for database queries
+    const query = "SELECT name, description, is_recyclable, packaging_material, brand FROM recyclable_household_items WHERE " + searchType + " = " + searchParam;
+    console.log(query);
     const [rows] = await connection.query(query);
     connection.end();
 
     if (rows.length > 0) {
       return rows;
     } else {
-      console.log('Error getting Data for brand:' + brand);
+      console.log('Error getting Data');
       return null;
+    }
+  } catch (error) {
+    console.error('Error retrieving data from the MySQL database', error);
+    return null; // Handle the error appropriately based on your use case
+  }
+}
+
+/*
+
+const normalSearch = async (brand) => {
+  try {
+    if (typeof brand === 'undefined') {
+      console.log("Brand variable is undefined");
+      return null;
+    }
+
+    //Look Aside Caching - Ref :https://www.youtube.com/watch?v=krSgKN-5DHs&list=LL&index=1&t=118s
+    const cacheKey = brand;//Design Cache Key Method
+    let cacheEntry = await redisClient.get(cacheKey);
+
+    if (cacheEntry) {
+      cacheEntry = JSON.parse(cacheEntry);
+      return { ...cacheEntry, 'source': 'cache' };
+    } else {
+      const connection = await createConnection();
+      //Adapted for rough search to work with AI
+      //Ref: https://www.w3schools.com/SQL/sql_like.asp
+      const query = "SELECT UPC, name, description, is_recyclable, packaging_material, brand FROM recyclable_household_items WHERE brand LIKE " + brand;
+      const [rows] = await connection.query(query);
+      connection.end();
+
+      if (rows.length > 0) {
+        redisClient.set(cacheKey, rows);
+        return { ...rows, 'source': 'database' };
+      } else {
+        console.log('Error getting Data for brand:' + brand);
+        return null;
+      }
     }
   } catch (error) {
     console.error('Error retrieving data from the MySQL database', error);
@@ -118,6 +193,7 @@ const nameSearch = async (name) => {
 }
 
 const upcSearch = async (upc) => {
+  //TO BE DISCONTINUED
   try {
     if (typeof upc === 'undefined') {
       console.log("UPC variable is undefined");
@@ -140,5 +216,5 @@ const upcSearch = async (upc) => {
     return null; // Handle the error appropriately based on your use case
   }
 }
-
+*/
 
